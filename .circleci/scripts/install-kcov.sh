43d75d36a04825bf51ebdee544700c74ebf2b5f1
Zach Bush <zach@zmbush.com>
2019-04-19 09:00:24 -0700

    [test] Add kcov reports

---
+++ .circleci/scripts/install-kcov.sh
@@ -0,0 +1,13 @@
+KCOV_VERSION=33
+
+sudo apt install -y cmake g++ pkg-config jq
+sudo apt install -y libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libiberty-dev
+
+wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV_VERSION.tar.gz && \
+    tar xzf v$KCOV_VERSION.tar.gz && \
+    rm v$KCOV_VERSION.tar.gz && \
+    cd kcov-$KCOV_VERSION && \
+    mkdir build && cd build && \
+    cmake .. && make
+
+cargo install cargo-kcov
