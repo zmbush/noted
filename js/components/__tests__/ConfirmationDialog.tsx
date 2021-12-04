d9a742fe572b2c290ed3a3ae2f57023d44686ed0
Zach Bush <zach@zmbush.com>
2021-12-04 15:49:25 -0800

    [test] Switch ConfirmationDialog test to @testing-library/react

--- js/components/__tests__/ConfirmationDialog.tsx
+++ js/components/__tests__/ConfirmationDialog.tsx
@@ -5,15 +5,50 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-import { shallow } from 'enzyme';
-
 import * as React from 'react';
+
+import { render } from 'components/test-utils';
 
 import ConfirmationDialog from '../ConfirmationDialog';
 
 describe('<ConfirmationDialog />', () => {
-  test('matches snapshot', () => {
-    const wrapper = shallow(<ConfirmationDialog open />);
-    expect(wrapper).toMatchSnapshot();
+  test('Renders as expected', async () => {
+    const { rerender, queryByText, getByText } = render(<ConfirmationDialog open={false} />);
+    expect(queryByText('Are you sure?')).toBeNull();
+    expect(queryByText('Yes')).toBeNull();
+    expect(queryByText('No')).toBeNull();
+
+    rerender(<ConfirmationDialog open />);
+    expect(getByText('Are you sure?')).toMatchInlineSnapshot(`
+      <div
+        class="MuiDialogContent-root css-ypiqx9-MuiDialogContent-root"
+      >
+        Are you sure?
+      </div>
+    `);
+    expect(getByText('Yes')).toMatchInlineSnapshot(`
+      <button
+        class="MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButtonBase-root  css-1e6y48t-MuiButtonBase-root-MuiButton-root"
+        tabindex="0"
+        type="button"
+      >
+        Yes
+        <span
+          class="MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root"
+        />
+      </button>
+    `);
+    expect(getByText('No')).toMatchInlineSnapshot(`
+      <button
+        class="MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButtonBase-root  css-1e6y48t-MuiButtonBase-root-MuiButton-root"
+        tabindex="0"
+        type="button"
+      >
+        No
+        <span
+          class="MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root"
+        />
+      </button>
+    `);
   });
 });
