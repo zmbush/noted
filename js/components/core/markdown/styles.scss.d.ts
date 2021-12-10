7f20954272808280814a7f58ba7376fa722d916e
Zach Bush <zach@zmbush.com>
2021-12-09 21:34:06 -0800

    [refactor] Refactor markdown components and fix print mode

---
+++ js/components/core/markdown/styles.scss.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const info: string;
+export const markdown: string;
+export const noticeBlock: string;
+export const tip: string;
+export const warning: string;
